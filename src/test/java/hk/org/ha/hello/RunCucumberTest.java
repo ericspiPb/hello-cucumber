f1a8bc4c89aca5973d46799172a2d8c028252ea6
EricChan <ckc261@HA11-PAM-W05.corp.ha.org.hk>
2020-12-08 16:59:54 +0800

    init project follow tutorial

---
+++ src/test/java/hk/org/ha/hello/RunCucumberTest.java
@@ -0,0 +1,9 @@
+package hk.org.ha.hello;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(plugin = {"pretty"})
+public class RunCucumberTest {}
